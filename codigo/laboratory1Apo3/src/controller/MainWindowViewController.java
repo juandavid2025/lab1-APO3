89e5750c9261a527d2ef5bb6242a35b557d1fdb6
juandavid2025 <juan.aguirre1@correo.icesi.edu.co>
2019-02-03 16:45:12 -0500

    agragadas carpetas codigo y javadoc, ademas se agrega el proyecto de eclipse con la interfaz en la carpeta codigo

---
+++ codigo/laboratory1Apo3/src/controller/MainWindowViewController.java
@@ -0,0 +1,46 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class MainWindowViewController implements Initializable{
+
+	
+	@FXML
+    private Button algorithm1Button;
+
+    @FXML
+    private Button algorithm2Button;
+
+    @FXML
+    private Button getRootsButton;
+
+    @FXML
+    void algorithm1Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void algorithm2Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getRootsClicked(ActionEvent event) {
+
+    }
+	
+	
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
