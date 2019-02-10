ecee9c9725f66d95cc97f7b31556d9cdb320c044
Lize756 <lize756cas@gmail.com>
2019-02-09 21:10:23 -0500

    added newton method

--- codigo/laboratory1Apo3/src/model/Calculator.java
+++ codigo/laboratory1Apo3/src/model/Calculator.java
@@ -2,6 +2,10 @@ package model;
 
 import java.util.Random;
 
+import org.lsmp.djep.djep.DJep;
+import org.nfunk.jep.Node;
+import org.nfunk.jep.ParseException;
+
 /**
  * 
  *
@@ -35,5 +39,77 @@ public class Calculator {
 		
 		return coefficients;
 	}
+	
+	/*
+	 * iThis method calculate the roots of a polynomial.
+	 * 
+	 * @param -function: is the polynomial to be evaluated.
+	 * 
+	 * @param -pZero: it is the initial point of approach to the root.
+	 * 
+	 * @pre: el String function It must not be empty.
+	 * 
+	 * @return the root of the polynomial.
+	 **/
+
+	public double newthon_Raphson(String function, double pZero) {
+		DJep Function = new DJep();
+		double raiz = 0;
+		boolean cont = false;
+		double dx = 0;
+		double fx = 0;
+		double p = 0;
+		String derivative = "";
+
+		Function.addStandardFunctions();
+		Function.addStandardConstants();
+		Function.addComplex();
+		Function.setAllowUndeclared(true);
+		Function.setAllowAssignment(true);
+		Function.setImplicitMul(true);
+		Function.addStandardDiffRules();
+
+		try {
+
+			// Derivadade la funcion
+			Node node = Function.parse(function);
+			Node diff = Function.differentiate(node, "x");
+			Node sim = Function.simplify(diff);
+			derivative = Function.toString(sim);
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		while (cont == false) {
+
+			try {
+				// Solucion de la derivada
+				Function.addVariable("x", pZero);
+				Node x = Function.parse(derivative);
+				Node f = Function.parse(function);
+
+				if (Function.evaluate(f) != null) {
+					fx = Double.valueOf(Function.evaluate(f).toString());
+					dx = Double.valueOf(Function.evaluate(x).toString());
+				}
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+
+			p = pZero - (fx / dx);
+			double error = (p - pZero) / p;
+
+			if (error <= 0.05) {
+				raiz = p;
+				cont = true;
+			} else {
+				pZero = p;
+			}
+		}
+
+		return raiz;
+	}
+
 	
 }
